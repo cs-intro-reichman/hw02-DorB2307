35af0eeae42fb85934cc273357e70af53fef8a97
DorB2307 <dor.barnoach@post.runi.ac.il>
2023-12-25 16:34:39 +0200

    Test2

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -6,9 +6,10 @@ public class DamkaBoard {
 		int n = Integer.parseInt(args[0]);
 		for (int i = 0; i < n; i++) {
 			if (i % 2 == 1) System.out.print(" ");
-			for (int j = 0; j < n; j++) {
+			for (int j = 0; j < n - 1; j++) {
 				System.out.print("* ");
 			}
+			System.out.print("*");
 			System.out.println();
 		}
 	}
